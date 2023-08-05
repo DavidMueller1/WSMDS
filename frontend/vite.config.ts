7d6575ff98bb6970cfca11392e61f81029f395d3
DavidMueller1 <david.muelle2@web.de>
2023-08-05 21:30:05 +0200

    Updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,40 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import * as fs from 'fs';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-		},
-		proxy: {}
-	}
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	if (!env.PRODUCTION)
+		return {
+			plugins: [sveltekit()]
+		};
+
+	return {
+		plugins: [sveltekit()],
+		server: {
+			https: {
+				key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+				cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
+			},
+			proxy: {
+				'/api': {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					secure: false,
+					rewrite: (path) => path.replace(/^\/api/, '')
+				}
+			}
+		}
+	};
 });
+
+// export default defineConfig({
+// 	plugins: [sveltekit()],
+// 	server: {
+// 		https: {
+// 			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+// 			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
+// 		},
+// 		proxy: {}
+// 	}
+// });
