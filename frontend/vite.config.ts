01a40ec9a6a8f3d28becc162460cd737dff29ab4
DavidMueller1 <david.muelle2@web.de>
2023-08-05 22:16:36 +0200

    Modified config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import * as fs from 'fs';
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
-	if (!env.PRODUCTION)
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	if (process.env.PRODUCTION)
 		return {
 			plugins: [sveltekit()]
 		};
